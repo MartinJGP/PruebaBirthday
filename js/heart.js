3605605af227f75db8e8ca7a14d0f76b87645767
Martin <martincasona085@gmail.com>
2024-07-27 20:25:33 -0500

    first

--- js/heart.js
+++ js/heart.js
@@ -57,9 +57,9 @@ Math.getDegree = function( x1, y1, x2, y2 ) {
 /*
  * Dot Object
  */
-var Dot = function( opts ) {
+var Dot = function(opts) {
     this.color = opts.color;
-    this.size = opts.size || 4; // Default size is 4 if not provided
+    this.size = window.innerWidth < 480 ? 12 : window.innerWidth < 768 ? 3 : 12; // Adjust size based on screen size
     this.x = 0;
     this.y = 0;
     this.dest_x = (opts.dest_x - 75);
@@ -97,7 +97,7 @@ FX.setFullscreen = function() {
  */
 // Update the FX.handleMouseEvent function to handle both mouse and touch events
 FX.handleMouseEvent = function(e, power) {
-    var radius = 150,
+    var radius = window.innerWidth < 480 ? 50 : window.innerWidth < 768 ? 100 : 150, // Adjust radius based on screen size
         k = FX.dots.length,
         i = 0,
         mx, my,
@@ -146,15 +146,15 @@ FX.canvas.addEventListener('touchstart', function(e) { FX.handleMouseEvent(e, 1)
 FX.createHeart = function() {
     var coords = [[1,4],[1,5],[1,6],[1,7],[1,8],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[2,9],[3,2],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[3,9],[3,10],[4,2],[4,3],[4,4],[4,5],[4,6],[4,7],[4,8],[4,9],[4,10],[4,11],[5,2],[5,3],[5,4],[5,5],[5,6],[5,7],[5,8],[5,9],[5,10],[5,11],[6,2],[6,3],[6,4],[6,5],[6,6],[6,7],[6,8],[6,9],[6,10],[6,11],[6,12],[7,3],[7,4],[7,5],[7,6],[7,7],[7,8],[7,9],[7,10],[7,11],[7,12],[8,3],[8,4],[8,5],[8,6],[8,7],[8,8],[8,9],[8,10],[8,11],[8,12],[8,13],[9,2],[9,3],[9,4],[9,5],[9,6],[9,7],[9,8],[9,9],[9,10],[9,11],[9,12],[10,2],[10,3],[10,4],[10,5],[10,6],[10,7],[10,8],[10,9],[10,10],[10,11],[11,2],[11,3],[11,4],[11,5],[11,6],[11,7],[11,8],[11,9],[11,10],[11,11],[12,2],[12,3],[12,4],[12,5],[12,6],[12,7],[12,8],[12,9],[12,10],[13,3],[13,4],[13,5],[13,6],[13,7],[13,8],[13,9],[14,4],[14,5],[14,6],[14,7],[14,8]],
         k = coords.length,
-        raster = 50, // Increase the raster value to scale up the heart
+        raster = window.innerWidth < 480 ? 20 : window.innerWidth < 768 ? 30 : 50, // Adjust raster value based on screen size
         i = 0;
 
     for( ;i<k;i=i+1 ) {
         FX.dots.push( new Dot({
-            dest_x : coords[i][0] * raster - (6 * raster), // Center horizontally
-            dest_y : coords[i][1] * raster - (6 * raster), // Center vertically
+            dest_x : coords[i][0] * raster - (5 * raster), // Center horizontally
+            dest_y : coords[i][1] * raster - (5 * raster), // Center vertically
             color: FX.config.color,
-            size: 35 // Keep the size of each dot the same
+            size: 50 // Keep the size of each dot the same
         }) );
     }
 };
@@ -183,6 +183,7 @@ FX.loop = function() {
 
     requestAnimFrame( FX.loop );
 };
+
 
 /*
  * Event bindings
